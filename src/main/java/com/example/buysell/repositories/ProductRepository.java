b05f028e68a022388b63331d26391fa970a4799b
Rasul <76030734+Rasul-max-oss@users.noreply.github.com>
2023-03-21 14:09:09 +0800

    Service setupе,adding save and delete methods

--- src/main/java/com/example/buysell/repositories/ProductRepository.java
+++ src/main/java/com/example/buysell/repositories/ProductRepository.java
@@ -3,6 +3,8 @@ package com.example.buysell.repositories;
 import com.example.buysell.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+import java.util.List;
 
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product>findByTitle(String title);
 }
