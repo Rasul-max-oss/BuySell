7cb84d957359069eea31ece568d36804250e0c02
Rasul <76030734+Rasul-max-oss@users.noreply.github.com>
2023-03-21 13:50:21 +0800

    create an interface for the databasе

---
+++ src/main/java/com/example/buysell/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.buysell.repositories;
+
+import com.example.buysell.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+}
